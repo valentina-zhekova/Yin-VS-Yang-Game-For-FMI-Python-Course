a7cc718b64b90ad895ca0324ced5fc019e5b5c7a
valentina-zhekova <valentina.p.zhekova@gmail.com>
2015-07-10 21:25:51 +0300

    player class implemented

--- player.py
+++ player.py
@@ -1,12 +1,18 @@
 class Player:
 
-    def __init__(self):
-        self.stones = []  # list of (row, col) indexes
-        self.stones_count = 0
-        self.stone_type = None
+    def __init__(self, sign):
+        self.stones = []
+        self.sign = sign
 
     def add_stone(self, row, col):
-        pass
+        stone = (row, col)
+        if stone not in self.stones:
+            self.stones.append(stone)
 
     def remove_stone(self, row, col):
-        pass
+        stone = (row, col)
+        if stone in self.stones:
+            self.stones.remove(stone)
+
+    def count_of_stones(self):
+        return len(self.stones)
